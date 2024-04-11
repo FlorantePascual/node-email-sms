5689cde4937e2eb711f848646bfbb27d9715df38
Florante Pascual <florante.pascual@gmail.com>
2024-04-11 18:54:58 -0400

    Generic constants

--- functions/src/helpers/constants/email-constants.ts
+++ functions/src/helpers/constants/email-constants.ts
@@ -1,5 +1,5 @@
 // Customization: Replace with your own email settings
-export const companyEmailTo = 'florante@cloudsterling.com';
-export const companyName = 'Cloud Sterling';
+export const companyEmailTo = 'harvey@pearson-spectre.com';
+export const companyName = 'Pearson Spectre';
 export const teamName = 'Customer Service';
-export const botName = 'Sterling Bot';
+export const botName = 'MikeRossBot';
